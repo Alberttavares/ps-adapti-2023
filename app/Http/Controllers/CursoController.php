3f29852073895724bc00316fe3a5bdc53580946d
Albert Rocha Tavares <gerenciamentoalberttavares@gmail.com>
2023-11-23 16:48:20 -0300

    Primeiro commit

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -2,39 +2,64 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreCursoRequest;
 
-use App\Models\Categoria;
+use App\Models\Curso;
 
 class CursoController extends Controller
 {
+    private $cursos;
 
+    public function __construct(Curso $curso) {
+        $this->cursos = $curso;
+    }
 
 
     public function index()
     {
-        return view('admin.curso.index');
+        $cursos = $this->cursos->all();
+        return view(('admin.curso.index'), compact('cursos'));
     }
 
 
     public function create()
     {
+        return view('admin.curso.crud');
     }
 
 
-    public function store(Request $request)
+    public function store(StoreCursoRequest $request)
     {
+        $data = $request->all();
+        $this->cursos->create($data);
+        return redirect()->route('curso.index')->with('success', 'Curso cadastrado com sucesso!'); 
+
+    }
+
+    public function show($id){
+        $curso = $this->cursos->find($id);
+        return response()->json($curso);
     }
 
     public function edit($id)
     {
+        $curso = $this->cursos->find($id);
+        return view('admin.curso.crud', compact('curso'));
     }
 
-    public function update(Request $request, $id)
+    public function update(StoreCursoRequest $request, $id)
     {
+        $data = $request->all();
+        $curso = $this->cursos->find($id);
+        $curso->update($data);
+        return redirect()->route('curso.index')->with('success', 'Curso atualizado com sucesso!'); 
     }
 
     public function destroy($id)
     {
+        $curso = $this->cursos->find($id);
+        $curso->delete();
+        return redirect()->route('curso.index')->with('success', 'Curso deletado com sucesso!');
+
     }
 }
--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -1,1 +1,13 @@
-{{-- Front-end começa aqui! --}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{ asset('site/css/styles.css') }}">
+  <title>Document</title>
+</head>
+<body>
+  <h1>ALBERT</h1>
+  <script src="{{ asset('site/js/scripts.js') }}"></script>
+</body>
+</html>
