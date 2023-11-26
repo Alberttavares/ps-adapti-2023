5d309090eef6472b9a575719551fd1b7a5f21e0b
Albert Rocha Tavares <gerenciamentoalberttavares@gmail.com>
2023-11-25 22:23:40 -0300

    Terceiro commit

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -3,14 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Curso;
+use App\Models\Aluno;
 
 
 class SiteController extends Controller
 {
+    private $cursos;
+    private $alunos;
+
+    public function __construct(Curso $curso, Aluno $aluno)
+    {
+        $this->alunos = $aluno;
+        $this->cursos = $curso;
+    }
 
     public function index()
     {
-        return view('site.index');
+        $cursos = $this->cursos->all();
+        $alunos = $this->alunos->all();
+
+        return view('site.index', compact('cursos', 'alunos'));
     }
 
 
--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -2,15 +2,40 @@
 <html lang="en">
 
 <head>
+
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="{{ asset('site/css/styles.css') }}">
+
   <title>OW recruitment</title>
+
 </head>
 
 <body>
-  <h1>ALBERT</h1>
+
+  <header>
+    <div class="teste">
+      ALBERT
+      @foreach ($cursos as $curso)
+      <tr>
+        <td>{{ $curso->id }}</td>
+        <td>{{ $curso->curso }}</td>
+      </tr>
+      @endforeach
+
+      @foreach ($alunos as $aluno)
+      <tr>
+        <td>{{ $aluno->id }}</td>
+        <td>{{ $aluno->nome }}</td>
+      </tr>
+      @endforeach
+    </div>
+  </header>
+
   <script src="{{ asset('site/js/scripts.js') }}"></script>
+
 </body>
 
 </html>
